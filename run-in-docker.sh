473c69ca039de3cbed34114ff967d4cce875a1d0
Ichinose Shogo <shogo82148@gmail.com>
2021-05-18 14:15:34 +0900

    bump Go v1.16.4

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -5,4 +5,4 @@ docker run --rm -it \
     -e GO111MODULE=on \
     -v "$CURRENT/.mod":/go/pkg/mod \
     -v "$CURRENT":/go/src/github.com/shogo82148/mackerel-cloudwatch-forwarder \
-    -w /go/src/github.com/shogo82148/mackerel-cloudwatch-forwarder golang:1.16.0 "$@"
+    -w /go/src/github.com/shogo82148/mackerel-cloudwatch-forwarder golang:1.16.4 "$@"
