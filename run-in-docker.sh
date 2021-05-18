958488c73d1a8857cf43e99e37bf14ec1410bfa8
Ichinose Shogo <shogo82148@gmail.com>
2021-05-18 15:01:27 +0900

    create a docker volume for caching

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 CURRENT=$(cd "$(dirname "$0")" && pwd)
+docker volume create mackerel-cloudwatch-forwarder-cache > /dev/null 2>&1
 docker run --rm -it \
     -e GO111MODULE=on \
-    -v "$CURRENT/.mod":/go/pkg/mod \
+    -v mackerel-cloudwatch-forwarder-cache:/go/pkg/mod \
     -v "$CURRENT":/go/src/github.com/shogo82148/mackerel-cloudwatch-forwarder \
     -w /go/src/github.com/shogo82148/mackerel-cloudwatch-forwarder golang:1.16.4 "$@"
