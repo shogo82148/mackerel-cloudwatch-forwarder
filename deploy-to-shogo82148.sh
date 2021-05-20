753d29c3eabf29110dfac69dbfef8bd102a8cb6f
Ichinose Shogo <shogo82148@gmail.com>
2021-05-20 11:03:25 +0900

    aff ForwardSettings

--- deploy-to-shogo82148.sh
+++ deploy-to-shogo82148.sh
@@ -7,8 +7,11 @@ sam package \
     --template-file template.yaml \
     --output-template-file packaged-test.yaml \
     --s3-bucket shogo82148-test
+ForwardSettings='[{"service":"shogo82148","name":"mackerel-cloudwatch-forwarder.duration","stat":"Sum","metric":["AWS/Lambda","Duration","FunctionName","mackerel-cloudwatch-forwarder-test-Forwarder-oDTgAaRmNR4h"]}]'
 sam deploy \
     --template-file packaged-test.yaml \
     --stack-name mackerel-cloudwatch-forwarder-test \
     --capabilities CAPABILITY_IAM \
-    --parameter-overrides ParameterName=/development/api.mackerelio.com/headers/X-Api-Key
+    --parameter-overrides \
+        ParameterName=/development/api.mackerelio.com/headers/X-Api-Key \
+        ForwardSettings="'$ForwardSettings'"
